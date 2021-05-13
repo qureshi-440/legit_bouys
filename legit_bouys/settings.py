8b054c174e59354df346bf101a8109dc1cc447df
Mohammed Farhan <mohammed045farhan@gmail.com>
2021-05-13 16:24:59 +0530

    updated allowed hosts

--- legit_bouys/settings.py
+++ legit_bouys/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS =  ['localhost','127.0.0.1']
+ALLOWED_HOSTS =  ['localhost','127.0.0.1','legit-blog.herokuapp.com']
 
 
 # Application definition
